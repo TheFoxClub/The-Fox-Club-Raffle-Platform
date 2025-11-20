c3d070c5bd22ba4a6586fbccb96713dadf398a6a
deadpoolmeta <deadpool.meta@gmail.com>
2025-11-20 20:35:17 +0545

    addition of build serve by backend

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,8 +1,4 @@
 const express = require("express");
 const router = express.Router();
-const AdminController = require("../controller/user-controller");
-const auth = require("../config/auth");
-
-router.get("/users/:id", Usercontroller.getOne);
 
 module.exports = router;
