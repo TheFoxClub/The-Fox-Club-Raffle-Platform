3191b09a06f4ece0413b92061d48f7b9a5e8f2c5
lu-husky <huskylunar7@gmail.com>
2025-11-24 13:30:03 +0545

    add model and migration for raffle

---
+++ server/migrations/20251124072830-create-raffle.js
@@ -0,0 +1,85 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("raffles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      title: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT(2000),
+        allowNull: true,
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      totalTickets: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      ticketPrice: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      ticketsSold: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+      },
+      tokenType: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        comment: "1 - SOL, 2 - USDC, 3 - mint address",
+      },
+      numberOfWinners: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 1,
+      },
+      startDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      endedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment:
+          "0 - upcoming, 1 - live, 2 - ended, 3 - cancelled, 4 - suspended",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("raffles");
+  },
+};
---
+++ server/models/raffle.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class raffle extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.User, {
+        foreignKey: "userId",
+        targetKey: "id",
+      });
+    }
+  }
+  raffle.init(
+    {
+      userId: DataTypes.INTEGER,
+      title: DataTypes.STRING(100),
+      description: DataTypes.TEXT(2000),
+      imageUrl: DataTypes.STRING,
+      totalTickets: DataTypes.INTEGER,
+      ticketPrice: DataTypes.DECIMAL(10, 2),
+      ticketsSold: DataTypes.INTEGER,
+      tokenType: DataTypes.INTEGER,
+      numberOfWinners: DataTypes.INTEGER,
+      startDate: DataTypes.DATE,
+      endDate: DataTypes.DATE,
+      endedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "raffle",
+    }
+  );
+  return raffle;
+};
