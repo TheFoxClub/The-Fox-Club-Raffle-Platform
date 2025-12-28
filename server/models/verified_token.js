32b33eb8cd1b52a14645aae72dc00875631f5074
lu-husky <huskylunar7@gmail.com>
2025-12-28 14:13:28 +0545

    refactor: add verified token model

---
+++ server/models/verified_token.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class verified_token extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  verified_token.init(
+    {
+      address: DataTypes.STRING,
+      name: DataTypes.STRING,
+      decimals: DataTypes.INTEGER,
+      isVerified: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "verified_token",
+    }
+  );
+  return verified_token;
+};
