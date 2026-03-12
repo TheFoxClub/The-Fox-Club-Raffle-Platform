5eb7f0ba5a050d0235342b19ca18fb38dda49459
setobread <breadseto222@gmail.com>
2026-03-12 15:53:09 +0545

    fix: added warning for featured raffle non refundable. And footer fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,9 +8,9 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="page-wrapper">
+    <div className="page-wrapper flex flex-col min-h-screen">
       <Header />
-      <main className="page-content">{children}</main>
+      <main className="page-content flex-1">{children}</main>
       <Footer />
     </div>
   );
